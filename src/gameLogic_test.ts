02d171755907bacd679ec0b64da82128157fce0f
Steven Adam <sna219@nyu.edu>
2017-03-28 18:15:36 -0400

    working unit test for p1 first move

--- src/gameLogic_test.ts
+++ src/gameLogic_test.ts
@@ -36,11 +36,12 @@ describe("In SnipeCity", function() {
       attackType: string,
       boardAfterMove: Board[],
       turnIndexAfterMove: number,
-      endMatchScores: number[]): void {
+      endMatchScores: number[],
+      turnCounts: number[]): void {
     let expectedMove:IMove = {
         turnIndex: turnIndexAfterMove,
         endMatchScores: endMatchScores,
-        state: {board: boardAfterMove, delta: {row: row, col: col, moveType: moveType, attackType: attackType}, gameOver: false, turnCounts: null}
+        state: {board: boardAfterMove, delta: {row: row, col: col, moveType: moveType, attackType: attackType}, gameOver: false, turnCounts: turnCounts}
       };
     let stateBeforeMove: IState = boardBeforeMove ? {board: boardBeforeMove, delta: null, gameOver: false, turnCounts: null} : null;
     let move: IMove = gameLogic.createMove(stateBeforeMove, row, col, moveType, turnIndexBeforeMove);
@@ -85,34 +86,34 @@ describe("In SnipeCity", function() {
     expect(angular.equals(move, expectedMove)).toBe(true);
   });
   
-  // it("P1 moving in 0x0 from initial state", function() {
-  //   expectMove(P1_TURN, null, 0, 0, 'move', '',
-  //     [[['', '', '', '', ''],
-  //     ['', '', '', '', ''],
-  //     ['', '', '', '', ''],
-  //     ['', '', '', '', ''],
-  //     ['', '', '', '', ''],
-  //     ['', '', '', '', '']], 
-  //     [['', '', '', '', ''],
-  //     ['', '', '', '', ''],
-  //     ['', '', '', '', ''],
-  //     ['', '', '', '', ''],
-  //     ['', '', '', '', ''],
-  //     ['', '', '', '', '']],
-  //     [['P', '', '', '', ''],
-  //     ['', '', '', '', ''],
-  //     ['', '', '', '', ''],
-  //     ['', '', '', '', ''],
-  //     ['', '', '', '', ''],
-  //     ['', '', '', '', '']],
-  //     [['', '', '', '', ''],
-  //     ['', '', '', '', ''],
-  //     ['', '', '', '', ''],
-  //     ['', '', '', '', ''],
-  //     ['', '', '', '', ''],
-  //     ['', '', '', '', '']]], 
-  //     P2_TURN, NO_ONE_WINS);
-  // });
+  it("P1 moving in 0x0 from initial state", function() {
+    expectMove(P1_TURN, null, 0, 0, 'move', '',
+      [[['', '', '', '', ''],
+      ['', '', '', '', ''],
+      ['', '', '', '', ''],
+      ['', '', '', '', ''],
+      ['', '', '', '', ''],
+      ['', '', '', '', '']], 
+      [['', '', '', '', ''],
+      ['', '', '', '', ''],
+      ['', '', '', '', ''],
+      ['', '', '', '', ''],
+      ['', '', '', '', ''],
+      ['', '', '', '', '']],
+      [['P', '', '', '', ''],
+      ['', '', '', '', ''],
+      ['', '', '', '', ''],
+      ['', '', '', '', ''],
+      ['', '', '', '', ''],
+      ['', '', '', '', '']],
+      [['', '', '', '', ''],
+      ['', '', '', '', ''],
+      ['', '', '', '', ''],
+      ['', '', '', '', ''],
+      ['', '', '', '', ''],
+      ['', '', '', '', '']]], 
+      P2_TURN, NO_ONE_WINS, [1,0]);
+  });
 
   // it("P2 attacking in 0x1", function() {
   //   expectMove(P2_TURN,
--- ts_output_readonly_do_NOT_change_manually/src/gameLogic_test.js
+++ ts_output_readonly_do_NOT_change_manually/src/gameLogic_test.js
@@ -20,11 +20,11 @@ describe("In SnipeCity", function () {
             throw new Error("We expect an illegal move, but createMove didn't throw any exception!");
         }
     }
-    function expectMove(turnIndexBeforeMove, boardBeforeMove, row, col, moveType, attackType, boardAfterMove, turnIndexAfterMove, endMatchScores) {
+    function expectMove(turnIndexBeforeMove, boardBeforeMove, row, col, moveType, attackType, boardAfterMove, turnIndexAfterMove, endMatchScores, turnCounts) {
         var expectedMove = {
             turnIndex: turnIndexAfterMove,
             endMatchScores: endMatchScores,
-            state: { board: boardAfterMove, delta: { row: row, col: col, moveType: moveType, attackType: attackType }, gameOver: false, turnCounts: null }
+            state: { board: boardAfterMove, delta: { row: row, col: col, moveType: moveType, attackType: attackType }, gameOver: false, turnCounts: turnCounts }
         };
         var stateBeforeMove = boardBeforeMove ? { board: boardBeforeMove, delta: null, gameOver: false, turnCounts: null } : null;
         var move = gameLogic.createMove(stateBeforeMove, row, col, moveType, turnIndexBeforeMove);
@@ -66,34 +66,32 @@ describe("In SnipeCity", function () {
         };
         expect(angular.equals(move, expectedMove)).toBe(true);
     });
-    // it("P1 moving in 0x0 from initial state", function() {
-    //   expectMove(P1_TURN, null, 0, 0, 'move', '',
-    //     [[['', '', '', '', ''],
-    //     ['', '', '', '', ''],
-    //     ['', '', '', '', ''],
-    //     ['', '', '', '', ''],
-    //     ['', '', '', '', ''],
-    //     ['', '', '', '', '']], 
-    //     [['', '', '', '', ''],
-    //     ['', '', '', '', ''],
-    //     ['', '', '', '', ''],
-    //     ['', '', '', '', ''],
-    //     ['', '', '', '', ''],
-    //     ['', '', '', '', '']],
-    //     [['P', '', '', '', ''],
-    //     ['', '', '', '', ''],
-    //     ['', '', '', '', ''],
-    //     ['', '', '', '', ''],
-    //     ['', '', '', '', ''],
-    //     ['', '', '', '', '']],
-    //     [['', '', '', '', ''],
-    //     ['', '', '', '', ''],
-    //     ['', '', '', '', ''],
-    //     ['', '', '', '', ''],
-    //     ['', '', '', '', ''],
-    //     ['', '', '', '', '']]], 
-    //     P2_TURN, NO_ONE_WINS);
-    // });
+    it("P1 moving in 0x0 from initial state", function () {
+        expectMove(P1_TURN, null, 0, 0, 'move', '', [[['', '', '', '', ''],
+                ['', '', '', '', ''],
+                ['', '', '', '', ''],
+                ['', '', '', '', ''],
+                ['', '', '', '', ''],
+                ['', '', '', '', '']],
+            [['', '', '', '', ''],
+                ['', '', '', '', ''],
+                ['', '', '', '', ''],
+                ['', '', '', '', ''],
+                ['', '', '', '', ''],
+                ['', '', '', '', '']],
+            [['P', '', '', '', ''],
+                ['', '', '', '', ''],
+                ['', '', '', '', ''],
+                ['', '', '', '', ''],
+                ['', '', '', '', ''],
+                ['', '', '', '', '']],
+            [['', '', '', '', ''],
+                ['', '', '', '', ''],
+                ['', '', '', '', ''],
+                ['', '', '', '', ''],
+                ['', '', '', '', ''],
+                ['', '', '', '', '']]], P2_TURN, NO_ONE_WINS, [1, 0]);
+    });
     // it("P2 attacking in 0x1", function() {
     //   expectMove(P2_TURN,
     //     [[['B', '', ''],
